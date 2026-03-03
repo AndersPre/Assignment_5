c555659ab8f8fe9148dbb66665e9fd2852432f3f
Linus Lau Thomsen <linuslauthomsen@gmail.com>
2026-03-03 10:59:08 +0100

    Idk it just want to add

--- assignment5/mini_java/src/main/java/dk/dtu/compute/course02324/mini_java/semantics/ProgramExecutorVisitor.java
+++ assignment5/mini_java/src/main/java/dk/dtu/compute/course02324/mini_java/semantics/ProgramExecutorVisitor.java
@@ -72,6 +72,8 @@ public class ProgramExecutorVisitor extends ProgramVisitor {
      *      (all operations with the respective types required in assignment must be defined above
      *      and added to the mapping below).
      */
+
+    
     final private Map<Operator, Map<Type, Function<List<Number>,Number>>> operatorFunctions = Map.ofEntries(
             entry(PLUS2, Map.ofEntries
                     (entry(INT, plus2int),
